4f7d049fef06feb6b2ec8477a52781469ec60a65
Farrukh-Murtaza <farrukhmurtaza1@gmail.com>
2026-09-25 14:33:46 -0400

    Implemented the schema and model requirements.

--- models/Product.js
+++ models/Product.js
@@ -2,8 +2,43 @@ const mongoose = require("mongoose");
 
 
 const productModel = mongoose.Schema({
+   name: {
+    type: String,
+    required: true,
+  },
 
+  description: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    min: [0.01, "Price must be greater than 0"],
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+
+  tags: {
+    type: [String],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 
 const Product = new mongoose.model("Product" , productModel)
+
+module.exports = Product;
